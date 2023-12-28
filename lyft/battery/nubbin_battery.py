50da28de1eb9e4b252d2e6a4c089d3d7c56b9232
NineNintyNine <duggiralavanimaheswari@gmail.com>
2023-12-27 23:16:27 -0800

    finacl changes done to unit testing

--- lyft/battery/nubbin_battery.py
+++ lyft/battery/nubbin_battery.py
@@ -1,11 +1,14 @@
 from battery.battery import Battery
 from datetime import datetime, timedelta
+from utils import add_years_to_date
 
 class NubbinBattery(Battery):
     def __init__(self,last_service_date, current_date):
         self.last_service_date = last_service_date;
         self.current_date = current_date
-        self.duration = timedelta(days=365 * 4)
+        # self.duration = timedelta(days=365 * 4)
           
     def needs_service(self)->bool:
-        return self.current_date - self.last_service_date >= self.duration
+        duration = add_years_to_date(self.last_service_date,4)
+        return duration < self.current_date
+    
--- lyft/battery/spindle_battery.py
+++ lyft/battery/spindle_battery.py
@@ -1,11 +1,13 @@
 from battery.battery import Battery
 from datetime import datetime, timedelta
+from utils import add_years_to_date
 
 class SpindleBattery(Battery):
     def __init__(self,last_service_date, current_date):
         self.last_service_date = last_service_date;
         self.current_date = current_date
-        self.duration = timedelta(days=365 * 2)
-
+        
     def needs_service(self)->bool:
-        return (self.current_date - self.last_service_date) >= self.duration
+            duration = add_years_to_date(self.last_service_date,2)
+            return duration < self.current_date
+    
